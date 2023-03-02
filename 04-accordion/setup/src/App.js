5e3ebe5b8e6755eb5d77b2d1de51128b86cc7338
Simone Calciolari <simone.clclr@gmail.com>
2023-03-02 12:50:24 +0100

    Complete FAQ Accordion project

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import questions from './data';
+
+import Question from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+
+        <section className='info'>
+          {questions.map(faq => 
+            <Question key={faq.id} {...faq} />  
+          )}
+        </section>
+      </div>      
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({title, info}) => {
+
+  const [toggled, setToggled] = useState(false)
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+
+        <button onClick={() => setToggled(!toggled)} className="btn">
+          {
+            toggled ? <AiOutlineMinus /> : <AiOutlinePlus/>
+          }
+        </button>
+      </header>
+
+      {
+        toggled && 
+        <p>
+          {info}
+        </p>
+      }
+    </article>
+  )
 };
 
 export default Question;
