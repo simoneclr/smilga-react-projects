d304eb95b9b0187d01bbcd82ebf6c857de717f1f
Simone Calciolari <simone.clclr@gmail.com>
2023-03-09 10:11:49 +0100

    Stock Photos: Add user info on each picture

--- 19-stock-photos/solution/src/Photo.js
+++ 19-stock-photos/solution/src/Photo.js
@@ -3,13 +3,24 @@ import React from 'react'
 const baseUrl = "https://picsum.photos/id/"
 const sizeUrl = "/810/540"
 
-const Photo = ({id}) => {
+const Photo = ({id, author, url: originalUrl}) => {
 
   const url = baseUrl + id + sizeUrl
 
   return (
     <article className="photo">
       <img src={url} alt="dog" />
+
+      <div className="photo-info">
+        <div>
+          <h4>{author}</h4>
+          <a href={originalUrl}>
+            View on unsplash
+          </a>
+        </div>
+
+        
+      </div>
     </article>
   )
 }
